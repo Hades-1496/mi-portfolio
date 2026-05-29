47ad7ecd52a4ee81101a94de559d65499bc20006
Hades <hades.otero@outlook.es>
2026-05-29 03:25:49 +0100

    Deploy corregido 0.3

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { GithubProvider } from './routes/GithubContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/mi-portfolio">
+    <HashRouter>
       <GithubProvider>
         <App />
       </GithubProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
